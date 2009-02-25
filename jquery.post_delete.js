25e96c9456871f88b3e5621852ddbaf4614b3eb2
Jacques Crocker <merbjedi@gmail.com>
2009-02-24 16:50:38 -0800

    Adding Post Delete plugin, that allows you to POST delete from links

---
+++ jquery.post_delete.js
@@ -0,0 +1,42 @@
+(function($j) {  
+  $j.fn.post_delete = function(opts) { 
+    // defaults
+    var opts = $j.extend({
+      confirm: function(el) {
+        // get confirmation message from attribute "confirm"
+        var confirmMessage = $j(el).attr("confirm");
+        if(confirmMessage != undefined && confirmMessage != "")
+        {
+          return confirm(confirmMessage);
+        }
+        else
+        {
+          return true;
+        }
+      }
+    }, opts);
+    
+    this.unbind("click");
+    this.live("click",function() {
+      
+      // check confirmation before submitting
+      var confirmed = true;
+      if(typeof(opts.confirm) == "function")
+      {
+        confirmed = opts.confirm(this);
+      }
+      
+      if(confirmed)
+      {
+        $j("<form method='POST'><input type='hidden' name='_method' value='delete' /><form>")
+          .attr("action", $j(this).attr("href"))
+          .insertAfter($j(this))
+          .submit();
+      }
+
+      return false;
+    });
+    
+    return this;
+  }; 
+})(jQuery);
